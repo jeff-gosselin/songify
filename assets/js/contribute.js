e3527c2fa9cdd96ae4d715fbdb16fe37484226da
jeff-gosselin <jgoz.email@gmail.com>
2019-02-13 17:57:06 -0500

    working on contribute

--- assets/js/contribute.js
+++ assets/js/contribute.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 	const contributeButton = document.querySelector('#contribute');
-	listSongsInProgress();
+
 
 });
 
@@ -11,13 +11,25 @@ function listSongsInProgress() {
 	container.style.color = 'rgb(61, 100, 122)';
 	container.innerHTML = `
 		<h1>Songs Currently Being Written</h1>
+		<ul class="song-list"></ul>
 	`;
 
 	fetch('http://localhost:3000/api/v1/songs')
 	.then(response => response.json())
 	.then(songs => songs.forEach(song => {
 
-		console.log(song.title);
+		if (song.complete === false) {
+
+			let songTitle = document.createElement('li');
+			let songList = document.querySelector('.song-list');
+			songTitle.setAttribute("data-id", `${song.id}`);
+			songTitle.style.listStyle = 'none';
+			songTitle.innerText = song.title;
+			songList.append(songTitle);
+
+			songTitle.addEventListener("click", showSongInProgress)
+
+		}
 
 	}))
 
