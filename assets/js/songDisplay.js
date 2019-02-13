35ed77f80c0a091c3347d4d136dff4510978cfa0
reduviidae <wiljago@gmail.com>
2019-02-13 15:05:38 -0500

    song display in process

--- assets/js/songDisplay.js
+++ assets/js/songDisplay.js
@@ -1,11 +1,34 @@
 const apiURL = `http://localhost:3000/api/v1/`;
+const container = document.querySelector("container")
 
 async function fetchSongs() {
   const request = await fetch(`${apiURL}songs`);
   const songs = await request.json();
-  return songs
+  return songs;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log(fetchSongs())
+async function fetchSections() {
+  const request = await fetch(`${apiURL}sections`)
+  const sections = await request.json();
+  return sections;
+}
+
+async function fetchSnippets() {
+  const request = await fetch(`${apiURL}snippets`)
+}
+
+async function makeSongCard() {
+  const songs = await fetchSongs();
+  const sections = await fetchSections();
+  const snippets = await fetchSnippets();
+
+  const songCard = document.createElement("div");
+  songCard.className = "song-card";
+
+};
+
+
+
+document.addEventListener('click', (e) => {
+  console.log(e.target)
 })
