ad48d39fbe38ff3bafa1a56e819405e124bc70fb
reduviidae <wiljago@gmail.com>
2019-02-14 08:46:09 -0500

    section & snippet form updated

--- assets/js/start.js
+++ assets/js/start.js
@@ -4,19 +4,21 @@ document.addEventListener("DOMContentLoaded", () =>{
   const sideBar = document.querySelector(".menu")
 
   async function fetchSongs() {
-    const request = await fetch(`${apiURL}songs`);
+    const request = await fetch(`${apiURL}songs.json`);
     const songs = await request.json();
     return songs;
   }
 
   async function fetchSections() {
-    const request = await fetch(`${apiURL}sections`)
+    const request = await fetch(`${apiURL}sections.json`)
     const sections = await request.json();
     return sections;
   }
 
   async function fetchSnippets() {
-    const request = await fetch(`${apiURL}snippets`)
+    const request = await fetch(`${apiURL}snippets.json`)
+    const snippets = await request.json();
+    return snippets
   }
 
   async function viewCompletedSongs() {
@@ -29,6 +31,21 @@ document.addEventListener("DOMContentLoaded", () =>{
 
   };
 
+  function addSectionWithSnippet(e) {
+    const songId = e.target.parentElement.id.slice(5);
+    fetch(`${apiURL}sections`, {
+      method: `POST`,
+      headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+      body: JSON.stringify({
+        song_id: songId,
+        section_type: 'verse'
+      })
+    }).then(console.log)
+  }
+
   async function showNewlyCreatedSong() {
     container.innerHTML = '';
     const songs = await fetchSongs();
@@ -37,22 +54,40 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 
     const title = document.createElement("h2");
+    title.id = `song-${lastSongId}`
+    title.class = `song-title`
     title.innerText = lastSong.title;
     container.append(title);
 
+    for(i = 0; i < 5; i++){
     const newSectionDiv = document.createElement("div");
-    newSectionDiv.id = "new-section";
+    newSectionDiv.class = `song-${lastSongId}`;
     const newSnippetForm = document.createElement("form");
-    newSnippetForm.class = "new-snippet-form"
+    newSnippetForm.class = "new-snippet-form";
     const newSnippetInput = document.createElement("input");
     newSnippetInput.class = "new-snippet-content";
+    const newSnippetDropDown = document.createElement("select");
+    newSnippetDropDown.class = "section-type";
+    const chorusOption = document.createElement("option");
+    chorusOption.value = "chorus";
+    chorusOption.innerText = "Chorus";
+    const verseOption = document.createElement("option");
+    verseOption.value = "verse";
+    verseOption.innerText = "Verse";
+
+
     const newSnippetButton = document.createElement("button");
     newSnippetButton.class = "submit-new-snippet";
-    container.appendChild(newSectionDiv);
-    newSectionDiv.appendChild(newSnippetForm);
-    newSnippetForm.appendChild(newSnippetInput);
-    newSnippetForm.appendChild(newSnippetButton);
+    container.append(newSectionDiv);
+    newSectionDiv.append(newSnippetForm);
+    newSnippetForm.append(newSnippetInput);
+    newSnippetForm.append(newSnippetDropDown);
+    newSnippetForm.append(newSnippetDropDown);
+    newSnippetDropDown.append(chorusOption);
+    newSnippetDropDown.append(verseOption);
+    newSnippetForm.append(newSnippetButton);
 
+    newSectionDiv.addEventListener('submit', e => {e.preventDefault(); addSectionWithSnippet(e)})}
   }
 
   function createSong(e) {
@@ -75,7 +110,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     songCard.className = "song-card";
     songCard.innerHTML = `
       <form id="new-song-form">
-      <input id="new-song-title" type="text" placeholder="Song Title">
+      <input id="new-song-title" type="text" placeholder="Song Title"><br>
       <button id="submit-new-song" type="submit" class="container-buttons">
       </form>
     `;
@@ -121,37 +156,37 @@ document.addEventListener("DOMContentLoaded", () =>{
 // const snippets = await fetchSnippets();
 // const theseSnippets = [];
 // theseSections.forEach(section => {
-  //   snippets.forEach(snippet => {
-    //     if(section.id === snippet.section_id) {
-      //       theseSnippets.push(snippet)
-      //     }
-      //   })
-      // })
+  //     snippets.forEach(snippet => {
+    //         if(section.id === snippet.section_id) {
+      //             theseSnippets.push(snippet)
+      //           }
+      //         })
+      //       })
 
-// theseSections.map(section => {
-  //   const sectionDiv = documentCreatElement("div");
-  //   sectionDiv.id = `section-${section.id}`;
-  //   sectionDiv.innerText = section.section_type
-  //   container.appendChild(sectionDiv)
-  // })
-  //
-  // theseSnippets.map(snippet => {
-    //   const snippetDiv = document.createElement("div");
-    //   const mySection = document.getElementById(`section-${snippet.section_id}`);
-    //   snippetDiv.id = `snippet-${snippet.id}`;
-    //   snippetDiv.innerText = snippet.content;
-    //   mySection.appendChild(snippetDiv);
-    // })
-    //
-    // theseSections.forEach(section => {
-      //   const sectionDiv = document.getElementById(`section-${section.id}`);
-      //   const newSnippetForm = document.createElement("form");
-      //   newSnippetForm.class = "new-snippet-form"
-      //   const newSnippetInput = document.createElement("input");
-      //   newSnippetInput.class = "new-snippet-content";
-      //   const newSnippetButton = documetn.createElement("button");
-      //   newSnippetButton.class = "submit-new-snippet";
-      //   sectionDiv.appendChild(newSnippetForm);
-      //   newSnippetForm.appendChild(newSnippetInput);
-      //   newSnippetForm.appendChild(newSnippetButton);
-      // })
+      // theseSections.map(section => {
+        //   const sectionDiv = documentCreatElement("div");
+        //   sectionDiv.id = `section-${section.id}`;
+        //   sectionDiv.innerText = section.section_type
+        //   container.appendChild(sectionDiv)
+        // })
+        //
+        // theseSnippets.map(snippet => {
+          //   const snippetDiv = document.createElement("div");
+          //   const mySection = document.getElementById(`section-${snippet.section_id}`);
+          //   snippetDiv.id = `snippet-${snippet.id}`;
+          //   snippetDiv.innerText = snippet.content;
+          //   mySection.appendChild(snippetDiv);
+          // })
+          //
+          // theseSections.forEach(section => {
+            //   const sectionDiv = document.getElementById(`section-${section.id}`);
+            //   const newSnippetForm = document.createElement("form");
+            //   newSnippetForm.class = "new-snippet-form"
+            //   const newSnippetInput = document.createElement("input");
+            //   newSnippetInput.class = "new-snippet-content";
+            //   const newSnippetButton = documetn.createElement("button");
+            //   newSnippetButton.class = "submit-new-snippet";
+            //   sectionDiv.appendChild(newSnippetForm);
+            //   newSnippetForm.appendChild(newSnippetInput);
+            //   newSnippetForm.appendChild(newSnippetButton);
+            // })
